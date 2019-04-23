3cecdf7ead180c73278f8fa90b68aab684fd874d
abubuckerm <48709844+abubuckerm@users.noreply.github.com>
2019-04-23 13:32:27 +1000

    Delete Question_4.py

--- Question_4.py
+++
@@ -1,5 +0,0 @@
-a=int(input('Enter number a :'))#enter a
-b=int(input('Enter number b :'))#enter b
-import random #import random module
-r= random.randint(a,b) #this function writes a value between a and b
-print (r) #prints that value
